9f9fe3422efec888edd4725a936126d1f8e38fa2
shorttheworld <akbardhannani@gmail.com>
2013-09-29 15:17:50 -0400

    Update FaceRecognizer.py

---
+++ Project/FaceRecognizer.py
@@ -0,0 +1,15 @@
+class FaceRecognizer:
+	# This class will be responsible for testing the learner
+	# on the image given. There will be multiple learners
+	# depending on the 'pin' the appropriate learner
+	# will be queried.
+	def __init__(self):
+		self.learner = None
+
+	#This method will query the correct learner and will return
+	# a boolean to indicate if the person was found or not.
+	def recognize(image, pin)
+
+
+
+	
