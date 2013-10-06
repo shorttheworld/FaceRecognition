386ad40504365b9a0f28d1c07d3dc78b4138e846
Hannah Lau <hannahlau7@gmail.com>
2013-10-06 12:52:01 -0400

    Adding Result class

---
+++ Result.py
@@ -0,0 +1,18 @@
+class Result:
+		def __init__(self):
+			self._message = "No result found."
+
+		def result(self):
+			return _message
+
+		def match(self):
+			self._message = "Match"
+
+		def noMatch(self):
+			self._message = "No Match"
+
+		def multipleFaces(self):
+			self._message = "Multple Faces"
+
+		def noFace(self):
+			self._message = "No Face"
