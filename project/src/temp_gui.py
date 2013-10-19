1732e0eb8a1bd32a0630da31fd43fa8a6269e0af
Hannah Lau <hannahlau7@gmail.com>
2013-10-19 17:14:49 -0400

    Updated GUI, removed dependency on goodOrBad, removed dead code when setting up video, separated intialization of GUI components, broke changing label from red to green

--- project/src/temp_gui.py
+++ project/src/temp_gui.py
@@ -44,6 +44,7 @@ def image_capture(queue):
    dim_b, dim_t, dim_l, dim_r = slice_frame(frame)
    
    count = 1
+
    while success != 0:
       success, frame = video.read()
       frame = frame[dim_b:dim_t, dim_l:dim_r]
@@ -51,14 +52,14 @@ def image_capture(queue):
       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       gray = cv2.equalizeHist(gray)
 
+      queue.put(frame) 
+
       if count < 25:
          rects = detect(gray, cascade, count)
    
          if rects != []:
             count = count + 1
-            #face_detected()
-
-      queue.put(frame) 
+            #detected_face()
 
 def slice_frame(frame):
    height = 350
@@ -83,11 +84,8 @@ def detect(img, cascade, count):
       rects = []
    else:
       crop_img = img[(rects[0][1]-20):(rects[0][1] + 204), (rects[0][0]-5):(rects[0][0]+179)]
-    
-      if len(crop_img) == 224 and len(crop_img[0]) == 184:
-         newX, newY = crop_img.shape[1]/2, crop_img.shape[0]/2
-         crop_img = cv2.resize(crop_img,(newX,newY))
-         cv2.imwrite("victim/"+str(count)+".jpg" , crop_img)
+      crop_img = cv2.resize(crop_img,(92,112))
+      cv2.imwrite("victim/" + str(count) + ".jpg" , crop_img)
 
    return rects   
 
@@ -109,61 +107,56 @@ def configure_main_window(root):
    root.geometry("800x500")
    root.resizable(width=False, height=False)
    root.configure(background="#EE8")
-   root.grid()
    root.title("In Yo Face")
 
 def configure_welcome_banner(root):
    welcome_font = tkFont.Font(family='Helvetica', size=12, weight='bold')
    
    welcome_frame = tk.LabelFrame(master=root, relief="ridge", bg="Black")
-   welcome_frame.grid(row=1, column=5, rowspan=3, columnspan=2)
-   welcome_frame.pack()
+   welcome_frame.grid(row=0, column=1, columnspan=2)
    
    welcome_message = 'Welcome to the In Yo Face Authentication System!'
    welcome_label = tk.Label(master=welcome_frame, text=welcome_message, font=welcome_font)
-   welcome_label.pack()
+   welcome_label.grid(row=0, column=1, columnspan=2)
 
-def configure_image_window(root):
+def configure_image_window(root, queue):
    image_label = tk.Label(master=root)
-   image_label.grid(row=1, column=1, rowspan=6, columnspan=3)
-   image_label.pack()
+   image_label.grid(row=3, column=0, rowspan=3)
 
-   return image_label
+   root.after(0, func=lambda: update_all(root, image_label, queue))
 
-def face_detected():
-   #print 'here'
-   #lf.config(bg = "Green")
-   print 'here'
-   lf_label.config(text="Face Detected", bg="Green")
-   print 'here'
+def configure_labels(root):
+   color = tk.StringVar()
+   color.set("red")
+
+   lf = tk.LabelFrame(master=root, bg=color.get(), bd=10, width=30, height=1)
+   lf.grid(row=3, column=1)
+
+   lf_label = tk.Label(master=lf, text='Searching for a face...', bg=color.get(), width=35)
+   lf_label.grid(row=3, column=1)
+
+def configure_buttons(root, p):
+   entry = tk.Entry(master=root, show='*', bg="White", fg="Black", takefocus=1, width=30)
+   entry.grid(row=4, column=1, sticky="n")
+   
+   enter_button = tk.Button(master=root, text='Enter', command=lambda: get_name(pin), bg="green", width=25, height=1)
+   enter_button.grid(row=4, column=1)
+
+   quit_button = tk.Button(master=root, text='Quit', command=lambda: quit(root, p), bg="red", width=25, height=1)
+   quit_button.grid(row=4, column=1, sticky="s")
 
 # Main method -------------------------------------------------------------------
 if __name__ == '__main__':
    queue = Queue()
    root = tk.Tk()
 
-   configure_main_window(root)
-   configure_welcome_banner(root)
-
-   image_label = configure_image_window(root)
-   
-   global lf
-   lf = tk.LabelFrame(master=root, bg="Red", bd=10, width=30, height=1)
-   lf.grid(row=7, column=1)
-   lf.pack()
-
-   color = tk.StringVar()
-   color.set('red')
-
-   global lf_labels
-   lf_label = tk.Label(master=root, text='Searching for a face...', bg=color.get(), width=35)
-   lf_label.pack()
-
    p = Process(target=image_capture, args=(queue,))
    p.start()
+
+   configure_main_window(root)
+   configure_welcome_banner(root)
+   configure_image_window(root, queue)
+   configure_labels(root)
+   configure_buttons(root, p)
    
-   quit_button = tk.Button(master=root, text='Quit',command=lambda: quit(root,p))
-   quit_button.pack()
-   
-   root.after(0, func=lambda: update_all(root, image_label, queue))
    root.mainloop()
