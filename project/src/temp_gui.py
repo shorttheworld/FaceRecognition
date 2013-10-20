9cc37cc43ffb6a31b3afd9ca0e2cf09e44072351
Hannah Lau <hannahlau7@gmail.com>
2013-10-20 15:42:24 -0400

    Enabled Enter button on GUI, fixed bug with changing label color

--- project/src/temp_gui.py
+++ project/src/temp_gui.py
@@ -2,6 +2,8 @@
 
 # Hannah's vesion of the gui
 
+import os
+
 import cv2
 import numpy as np
 
@@ -34,33 +36,17 @@ def quit(root, process):
    root.destroy()
 
 # Multiprocessing image processing functions ------------------------------------
-def image_capture(queue):
-   cascade_fn = "../metadata/haarcascade_frontalface_alt.xml"
-   cascade = cv2.CascadeClassifier(cascade_fn)
-
+def video_feed(queue):
    video = create_capture(0)
 
    success, frame = video.read()
    dim_b, dim_t, dim_l, dim_r = slice_frame(frame)
    
-   count = 1
-
    while success != 0:
       success, frame = video.read()
       frame = frame[dim_b:dim_t, dim_l:dim_r]
-
-      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      gray = cv2.equalizeHist(gray)
-
+      
       queue.put(frame) 
-
-      if count < 25:
-         rects = detect(gray, cascade, count)
-   
-         if rects != []:
-            count = count + 1
-            #Need to fix this method
-            #detected_face()
 
 def slice_frame(frame):
    height = 350
@@ -77,35 +63,78 @@ def slice_frame(frame):
 
    return (dim_b, dim_t, dim_l, dim_r)
 
-def detect(img, cascade, count):
+# Face detection ----------------------------------------------------------------
+def start_detection(queue, lf, lf_label):
+   cascade_fn = "../metadata/haarcascade_frontalface_alt.xml"
+   cascade = cv2.CascadeClassifier(cascade_fn)
+
+   sh('mkdir victim')
+   sh('rm victim/*')
+   
+   fileList = os.listdir(os.getcwd() + '/victim/')
+   num_pics = len(fileList)
+
+   while (num_pics < 25):
+      frame = queue.get()
+
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      gray = cv2.equalizeHist(gray)
+
+      rects = detect_face(gray, cascade, num_pics)
+
+      detected_face(lf, lf_label, num_pics)
+
+      fileList = os.listdir(os.getcwd() + '/victim/')
+      num_pics = len(fileList)
+
+def detect_face(img, cascade, count):
    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, 
       minSize=(30, 30), flags =cv2.CASCADE_SCALE_IMAGE) #CV_HAAR_SCALE_IMAGE
 
    if len(rects) == 0: 
       rects = []
    else:
+      #what is this??
       crop_img = img[(rects[0][1]-20):(rects[0][1] + 204), (rects[0][0]-5):(rects[0][0]+179)]
       crop_img = cv2.resize(crop_img,(92,112))
+      
       cv2.imwrite("victim/" + str(count) + ".jpg" , crop_img)
 
    return rects   
 
-def detected_face():
-   #This method should change the color of the label frame (lf) and label (lf_label)
-   #from red to green. It should also update the text of the label (lf_label)
-   
+def detected_face(lf, lf_label, num_pics):
+   color = lf['bg']
+
+   if (color != 'red' and num_pics == 0):
+      lf.config(bg="red")
+      lf_label.config(bg="red", text='Searching for face...')
+   elif (color != 'yellow' and 0 < num_pics and num_pics < 24):
+      lf.config(bg="yellow")
+      lf_label.config(bg="yellow", text='Matching face.')
+   elif (color != 'green' and num_pics == 24):
+      #name = db.get_name(db, '12345')
+      name = 'Hannah'
+      lf.config(bg="green")
+      lf_label.config(bg="green", text='Detected: ' + name)
+      # call Saman
+      
 # Database functions ------------------------------------------------------------
 def setup_db():
-   db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="FacialRecognition") 
-   global cur
-   cur = db.cursor() 
+   # mysql -u root -p FacialRecognition; root
+   mySQL = MySQLdb.connect(host="localhost", user="root", passwd="root", db="FacialRecognition") 
+   db = mySQL.cursor() 
 
-def get_name(pin):
-   cmd = "SELECT first_name, last_name FROM person WHERE pin=" + str(pin)
-   cur.execute(cmd)
-   person = cur.fetchone()
+def get_name(db, password):
+   if sanitize_input(password):
+      cmd = "SELECT first_name, last_name FROM person WHERE password=" + str(password)
+      db.execute(cmd)
+      person = db.fetchone()
 
-   print person[0] + " " + person[1]
+      return person[0] + " " + person[1]
+
+def sanitize_input(input):
+   return True
+   # Make sure input is not harmful to DB
 
 # Configure GUI components ------------------------------------------------------
 def configure_main_window(root):
@@ -124,44 +153,54 @@ def configure_welcome_banner(root):
    welcome_label = tk.Label(master=welcome_frame, text=welcome_message, font=welcome_font)
    welcome_label.grid(row=0, column=1, columnspan=2)
 
+def configure_labels(root):
+   color = tk.StringVar()
+   color.set("red")
+
+   global lf
+   lf = tk.LabelFrame(master=root, bg=color.get(), bd=10, width=30, height=1)
+   lf.grid(row=3, column=1)
+
+   global lf_label
+   lf_label = tk.Label(master=lf, text='Enter a password.', bg=color.get(), width=35)
+   lf_label.grid(row=3, column=1)
+
 def configure_image_window(root, queue):
    image_label = tk.Label(master=root)
    image_label.grid(row=3, column=0, rowspan=3)
 
    root.after(0, func=lambda: update_all(root, image_label, queue))
 
-def configure_labels(root):
-   color = tk.StringVar()
-   color.set("red")
-
-   lf = tk.LabelFrame(master=root, bg=color.get(), bd=10, width=30, height=1)
-   lf.grid(row=3, column=1)
-
-   lf_label = tk.Label(master=lf, text='Searching for a face...', bg=color.get(), width=35)
-   lf_label.grid(row=3, column=1)
-
-def configure_buttons(root, p):
+def configure_buttons(root, p, queue):
+   global entry
    entry = tk.Entry(master=root, show='*', bg="white", fg="black", takefocus=1, width=30)
    entry.grid(row=4, column=1, sticky="n")
    
-   enter_button = tk.Button(master=root, text='Enter', command=lambda: get_name(pin), bg="green", width=25, height=1)
+   enter_button = tk.Button(master=root, text='Enter', command=lambda: start_detection(queue, lf, lf_label), 
+      bg="green", width=25, height=1)
    enter_button.grid(row=4, column=1)
 
    quit_button = tk.Button(master=root, text='Quit', command=lambda: quit(root, p), bg="red", width=25, height=1)
    quit_button.grid(row=4, column=1, sticky="s")
+
+# Bash commands -----------------------------------------------------------------
+def sh(script):
+   os.system("bash -c '%s'" % script)
 
 # Main method -------------------------------------------------------------------
 if __name__ == '__main__':
    queue = Queue()
    root = tk.Tk()
 
-   p = Process(target=image_capture, args=(queue,))
-   p.start()
+   p = Process(target=video_feed, args=(queue,))
+
+   #db = setup_db()
 
    configure_main_window(root)
    configure_welcome_banner(root)
-   configure_image_window(root, queue)
    configure_labels(root)
-   configure_buttons(root, p)
+   configure_buttons(root, p, queue)
+   configure_image_window(root, queue)
    
+   p.start()
    root.mainloop()
