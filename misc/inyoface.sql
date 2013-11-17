3c969c7befa1dc739f564e14e5b21df8d6127e02
Solace Collestan <solace.coll@gmail.com>
2013-11-17 18:28:10 -0500

    Latest database files
    If importing over previous versions of the database on phpMyAdmin make
    sure you drop the database before you import this one.

--- misc/inyoface.sql
+++ misc/inyoface.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 05, 2013 at 05:27 AM
+-- Generation Time: Nov 17, 2013 at 11:25 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
@@ -30,7 +30,7 @@ USE `inyoface`;
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `USERNAME` varchar(65) NOT NULL,
-  `PASS_HASH` varchar(8192) NOT NULL,
+  `PASSWORD` varchar(8192) NOT NULL,
   PRIMARY KEY (`USERNAME`),
   UNIQUE KEY `USERNAME` (`USERNAME`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -39,20 +39,8 @@ CREATE TABLE IF NOT EXISTS `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`USERNAME`, `PASS_HASH`) VALUES
+INSERT INTO `admin` (`USERNAME`, `PASSWORD`) VALUES
 ('scollestan', 'asdf');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `learner`
---
-
-CREATE TABLE IF NOT EXISTS `learner` (
-  `LEARNER` longblob NOT NULL,
-  `TIME_STAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`TIME_STAMP`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -61,24 +49,12 @@ CREATE TABLE IF NOT EXISTS `learner` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `FIRST_NAME` varchar(20) NOT NULL,
-  `LAST_NAME` varchar(65) NOT NULL,
-  `PASSWORD` varchar(65) NOT NULL,
-  `IMG_PATH` varchar(200) NOT NULL,
+  `FIRST_NAME` varchar(255) NOT NULL,
+  `LAST_NAME` varchar(255) NOT NULL,
+  `PASSWORD` varchar(712) NOT NULL,
   PRIMARY KEY (`PASSWORD`),
-  UNIQUE KEY `USERNAME_2` (`PASSWORD`),
-  KEY `USERNAME` (`PASSWORD`),
-  KEY `USERNAME_3` (`PASSWORD`)
+  UNIQUE KEY `PASSWORD` (`PASSWORD`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`FIRST_NAME`, `LAST_NAME`, `PASSWORD`, `IMG_PATH`) VALUES
-('Chris', 'Adams', '1234', '/data/1234'),
-('sadf', 'asdf', 'asdf', '/data/asdf'),
-('Solace', 'Collestan', 'scollestan', 'ghads08w4et20eh/fasd\\ewahofi');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
