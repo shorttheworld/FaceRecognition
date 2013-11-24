b824f8c2154b58d3ba1b437281a48f317186e313
Solace Collestan <solace.coll@gmail.com>
2013-11-24 17:12:33 -0500

    New Server
    changes to server.py: no FTP added connect function.

--- Project/src/server.py
+++ Project/src/server.py
@@ -1,43 +1,24 @@
-#Server Connections for Database and FTP
+#Server Connections for Database
 
 import MySQLdb as SQL
-from ftplib import FTP
 
 
-
-class Server:
-	def __init__(self, host, user, passwd, ftpuser, ftppw):
-                self.host=host
-                self.ftpuser=ftpuser
-                self.ftppw=ftppw
-                
+class Server: 
+		
+	def connect(self, host, user, passwd):
 		self.db=SQL.connect(host=host, user=user,passwd=passwd,db='inyoface', port=3306)
 		self.cursor=self.db.cursor()
-		self.ftp=None
-		self.ftp=self.ftpConnect()
-		#self.ftp.prot_p()
 		
-	def addUser(self, fn, ln, username):
+	def addUser(self, fn, ln, username, piclist):
 		sql="INSERT INTO user (FIRST_NAME, LAST_NAME, USERNAME) VALUES (%s,%s, %s)"
 		self.cursor.execute(sql, (fn,ln,username))
 		self.db.commit()
-	
+		
 	def deleteUser(self, username):
 		sql="DELETE FROM user WHERE USERNAME='"+username+"'"
 		self.cursor.execute(sql)
 		self.db.commit()
-
-		'''
-		self.ftpConnect()
-		ftp.cwd("./data/")
-		self.ftp.rmd(pw)
-		'''
 	
-	def sendLearner(self, learner):
-		self.ftp_Connect()
-		ftp.cwd("./metadata/")
-		self.ftp.storbinary("STOR "+learner.name,learner)
-		
 	def addAdmin(self,user, passwd):
 		sql="INSERT INTO admin (USERNAME, PASSWORD) VALUES (%s, %s)"
 		self.cursor.execute(sql, (user, passwd))
@@ -59,9 +40,3 @@ class Server:
 		self.cursor.execute(sql)
 		self.db.commit()
 		return self.cursor.fetchall()
-		
-	def ftpConnect(self):
-                if self.ftp==None: #Create FTP object if none exists
-                        return FTP(self.host, self.ftpuser, self.ftppw)
-		self.ftp.login(self.host, self.ftpuser, self.ftppw) #Resestablish the ftp connection if necessary
-		
