9ca79f74d16800b3a5658691d0f8d1568fcbf4f5
cadams32 <cadams32@gatech.edu>
2013-12-08 23:26:50 -0500

    This time with the right SQL file
    Located in the misc folder

--- misc/inyoface.sql
+++ misc/inyoface.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 04, 2013 at 04:48 PM
+-- Generation Time: Dec 09, 2013 at 04:25 AM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
@@ -39,7 +39,9 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 INSERT INTO `admin` (`USERNAME`, `PASS_HASH`) VALUES
-('Garcon', 'colliefarms1');
+('admin', 'aeb37d91ec0137985476736428db4ba3675c47587df0d63318afc82335cc989fa9c065d2b97209b2d273bb09ad90812847203b432ef9d6dc3319545517b77380'),
+('Garcon', '5c901b0c73460cbd06429bd825c8fff85d0b7ba65fd05213aed188a071a072cd9a395a877cacdff2da3503eef092b0fac7b2982bd43d12c7498042a16c27bc42'),
+('Hannah', '6e5851a3b5c72e49f5a3ed405c1d93ad16b6b542846430e06037bbe8d47dab70a64b88374be2c229cb3152d4885ff822c42c122cd8112bce87b9e55c5da855d8');
 
 -- --------------------------------------------------------
 
@@ -54,15 +56,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_name` varchar(500) NOT NULL,
   `active` int(1) NOT NULL,
   PRIMARY KEY (`user_index`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=107 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_index`, `username`, `first_name`, `last_name`, `active`) VALUES
-(12, 'Rabkakbar', 'Akbar', 'Dhananni', 1),
-(13, 'Garcon', 'Chris', 'Adams', 1);
+(103, 'testpass', 'Saman', 'Shareghi', 1),
+(104, 'Hannah', 'Hannah', 'Lau', 1),
+(106, 'Garcon', 'Chris', 'Adams', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
